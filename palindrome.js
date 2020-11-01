ac41a03e769e303a59cd88dc11849b722626c435
Denise Tran <tdenise@users.noreply.github.com>
2020-11-01 15:55:37 -0800

    Initial commit

---
+++ palindrome.js
@@ -0,0 +1,49 @@
+// This program checks if the user's input is a palindrome and 
+// prints the reverse of the word.
+// before running the program run npm install prompt-sync
+
+var prompt = require('prompt-sync')();      //required for prompt method to work
+
+function palindromeChecker(){
+    var wordInput = prompt('Enter a string: ');
+    var lowercaseWord = wordInput.toLowerCase();
+    var reverseWord = [];
+
+    //Using JS inbuilt methods we can reverse the string and compare it to the string input
+    //split() method splits each character in the string into an array 
+    //reverse() method reverses the order of an array
+    //join() method concatenates an element of an array together into a single string
+    reverseWord = lowercaseWord.split('').reverse().join('');
+
+    //checks if reverseWord is equal to the original user input
+    if (reverseWord == lowercaseWord) {
+        console.log("The string "+ lowercaseWord +" is a palindrome.");
+        //prints the reverse of wordInput
+        console.log("The reverse of " + lowercaseWord + " is " + reverseWord);
+    } else {
+        console.log("The string "+ lowercaseWord +" is not a palindrome.");
+        //prints the reverse of wordInput
+        console.log("The reverse of " + lowercaseWord + " is " + reverseWord);
+        resetPrompt();
+    }
+}
+
+// calls palindromeChecker method again based on whether user response to resetPrompt() is yes or no
+//otherwise, remprompt. ask user if they want to retry again.
+function resetOptions(retryInput) {
+    if ("Yes" || "yes" || "y" || "Y") {
+        palindromeChecker()
+    } else if ("No" || "no" || "n" || "N") {
+        var retryInput = prompt("Have a nice day! ");
+    } else {
+        resetPrompt();
+    }
+}
+
+//prompts user to reset
+function resetPrompt() {
+    var retryInput = prompt("Would you like to retry? (Y/N) ");
+    resetOptions();
+}
+
+palindromeChecker();
